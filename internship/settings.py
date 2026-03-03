82187c17d9d2ffe1c14556b12a7693d1e6c73118
Harsha Hiremath <harshahiremath591@gmail.com>
2026-03-03 16:47:09 +0530

    added postgres support

--- internship/settings.py
+++ internship/settings.py
@@ -75,15 +75,13 @@ WSGI_APPLICATION = 'internship.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'internship',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
