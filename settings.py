d0d9e7e64f5ca034aa2712e8b11ad130efcfc424
harshahiremath367-a11y <harshahiremath367@gmail.com>
2026-03-07 16:31:30 +0530

    Fixed template path for week4_4

--- settings.py
+++ settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'internship.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'myapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
